2b5cb4f41837a8aca7ed50f9abe95405128ed8c1
Binazira <binazir.usenkanova@gmail.com>
2023-02-07 17:28:25 +0600

    add img

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import "./Header.scss";
+import weatherLogo from "../../assets/icons/weatherLogo.svg";
 
 const Header = () => {
   return (
     <header className="header">
+      <div>
+        <img src={weatherLogo} alt="weather-logo" />
+      </div>
       <h2> WEATHER FORECAST</h2>
     </header>
   );
