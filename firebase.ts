d534483f3af584452188f0928816255759673d47
Binazira <binazir.usenkanova@gmail.com>
2023-02-07 18:49:02 +0600

    added meta tag

---
+++ firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBYtWDADJH6CU_OeexBXspEU-4fc22ASQc",
+  authDomain: "weather-d3357.firebaseapp.com",
+  projectId: "weather-d3357",
+  storageBucket: "weather-d3357.appspot.com",
+  messagingSenderId: "170051582963",
+  appId: "1:170051582963:web:74cf680a124362b8e83086",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
